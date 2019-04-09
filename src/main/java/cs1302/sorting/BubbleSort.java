1d9678cac8b5343002a9b8c0628cd8752c39420a
HARRY GOELLER <goeller@nike.cs.uga.edu>
2019-04-09 12:13:59 -0400

    added bubbleSort

--- src/main/java/cs1302/sorting/BubbleSort.java
+++ src/main/java/cs1302/sorting/BubbleSort.java
@@ -5,5 +5,18 @@ public class BubbleSort
 
 
 
+    public static <T> void bubblsSort(T[] array, int lo, int hi, Comparator<T> c){
+        if (lo > hi){
+            System.out.println("invalid indices");
+            return;
+        }
+        int count = 0;
+        T[] unsorted = array;
+        while(hi>=lo){
+            array[count] = bubble(unsorted, 0, hi, Integer::compareTo);
+            hi--;
+            count++;
+        }
 
-}
+    }
+},
