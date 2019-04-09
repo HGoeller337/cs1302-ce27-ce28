28a63bcdce4c39311a2634430486c6d4409b7cbd
KEVIN WALSH <walsh@nike.cs.uga.edu>
2019-04-09 11:30:44 -0400

    Add src

---
+++ src/main/java/cs1302/sorting/App.java
@@ -0,0 +1,13 @@
+package cs1302.sorting;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
