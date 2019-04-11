f43e6ed50ed1e9643360478a2a0a1715e73a8e6c
KEVIN WALSH <walsh@nike.cs.uga.edu>
2019-04-11 12:13:53 -0400

    Implement selectionsort

--- src/main/java/cs1302/sorting/SelectionSort.java
+++ src/main/java/cs1302/sorting/SelectionSort.java
@@ -4,11 +4,11 @@ import java.util.Comparator;
 public class SelectionSort{
 
     public static void main (String[] args){
-        int[] test = {1, 3, 5, 7, 8, 2, 9};
+        Integer[] test = {1, 3, 5, 7, 8, 2, 9};
         for(int i = 0; i < test.length; i++){
             System.out.println(test[i]);
         }// for 
-        selectMin(test, 2, 5, new Comparator());
+        selectionSort(test, 2, 5, Integer::compareTo);
         for(int i = 0; i < test.length; i++){
             System.out.println(test[i]);
         }// for 
@@ -16,25 +16,24 @@ public class SelectionSort{
     
     public static <T> void selectMin(T[] array, int lo, int hi, Comparator<T> c)
         {
-            
              T[] temp = array;
             if(!(lo<=hi))
             {
                 System.out.println("indices invalid");
             }else
             {
-                for(int i = lo; i < hi; i++)
-                {
-                    for(int z = i+1; z < hi; z++){
-                        if(c.compare(temp[i], temp[z]) > 0){
-                            T tempT = temp[i]; 
-                            temp[i] = temp[z];
+                    for(int z = lo+1; z <= hi; z++){
+                        if(c.compare(temp[lo], temp[z]) > 0){
+                            T tempT = temp[lo]; 
+                            temp[lo] = temp[z];
                             temp[z] = tempT;
                         }// if
                     }// for
                 }//for
             } 
-
-        }
-    
+    public static <T> void selectionSort(T[] array, int lo, int hi, Comparator<T> c){
+        for(int i = lo; i <= hi; i++){
+            selectMin(array, i, hi, c);
+        }// for
+    }// selctionSort
 } //hewwo
