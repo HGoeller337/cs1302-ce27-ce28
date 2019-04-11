2628244a23bcad523bb9219352877fa562864249
Nikita Michaelson <nikita.michaelson@uga.edu>
2019-04-11 11:35:54 -0400

    selection setup

--- src/main/java/cs1302/sorting/SelectionSort.java
+++ src/main/java/cs1302/sorting/SelectionSort.java
@@ -1,5 +1,12 @@
 package cs1302.sorting;
 
-public class SelectionSort{
+public class SelectionSort
+{
+    public static <T> void selectMin(T[] array, int lo, int hi, Comparator<T> c)
+        {
+            
+            
 
+        }
+    
 } //hewwo
